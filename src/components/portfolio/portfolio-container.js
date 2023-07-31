b90bb368d48f898d05e484a9c858c9eb037176c5
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-07-31 16:48:29 +0200

    B_10_Intro_to_conditionals

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,6 +10,7 @@ export default class PortfolioContainer extends Component {
         
         this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoading: true,
             data: [
                 { title: "Quip", category: "eCommerce" },
                 { title: "Eventbrite", category: "Scheduling" },
@@ -36,6 +37,10 @@ export default class PortfolioContainer extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
                <h2>{this.state.pageTitle}</h2> 
