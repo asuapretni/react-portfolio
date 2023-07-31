7a14e75e38837c0536d7432f91528df1d5bfd24b
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-07-31 19:53:23 +0200

    C_03_nav_link_active_styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,12 +9,11 @@ export default class NavigationComponent extends Component {
   render() {
     return (
       <div>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
         <NavLink to="/about-me">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
 
-
-        <button>Contact</button>
-        <button>Blog</button>
         {false ? <button>Add Blog</button> : null }
       </div>
     );
