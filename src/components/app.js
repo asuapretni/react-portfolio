da57ada9d4dca10aefc5eb06f8592e5681d7bcf2
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-07-31 17:27:35 +0200

    B_11_Use_ternery_operators_in_jsx

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import moment from "moment";
 import 'moment/locale/es';
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
         <div>{moment().format('LLLL')}</div>
