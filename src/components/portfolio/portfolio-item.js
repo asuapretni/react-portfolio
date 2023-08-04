d077d324616d403a3fc3e8e60b1145dc3fa3a468
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-04 18:57:37 +0200

    D_13_refactoring_portfolio_item_to_use_destructuring

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
+        // Data that we'll need:
+        // - background image: thumb_image_url
+        // - logo 
+        // - description: description
+        // - id: id
+        // ['id', 'name', 'description', 'url', 'category', 'position', 'thumb_image_url', 'banner_image_url', 'logo_url', 'column_names_merged_with_images']
+    const { id, description, thumb_image_url, logo } = props.item;
     return (
         <div>
-            <h3>{props.title}</h3>
-            <h4>{props.des}</h4>
-            <h4>{props.url}</h4>
-
-            <Link to={`/portfolio/${props.title}`}>Link</Link>
+            <div>{description}</div>
+            <Link to={`/portfolio/${id}`}>Link</Link>
         </div>
     )
     
