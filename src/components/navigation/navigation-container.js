0f1755cfa210c27ee9163d3ebb2109bd72a82163
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-05 11:39:54 +0200

    E_03_building_initial_nav_flexbox_container

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,13 +8,16 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-        <NavLink to="/about-me">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
+      <div className='nav-wrapper'>
+        <div className='left-side'>
+          <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+          <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+          <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+          <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
 
-        {false ? <button>Add Blog</button> : null }
+          {false ? <button>Add Blog</button> : null }
+        </div>
+        <div className='right-side'>CML</div>
       </div>
     );
     }
