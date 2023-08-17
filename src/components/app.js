b5dc175503e92b71d8c0d92da84e7cb3979b2e8d
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-17 10:18:48 +0200

    G_02_building_a_secure_class_component

--- src/components/app.js
+++ src/components/app.js
@@ -5,12 +5,13 @@ import {
   Route
 } from 'react-router-dom';
 import axios from 'axios';
-
+import PortfolioManager from './pages/portfolio-manager';
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
@@ -78,7 +79,7 @@ export default class App extends Component {
   }
 
   authorizedPages() {
-    return [<Route path="/blog" component={Blog} />]
+    return [<Route path="/portfolio-manager" component={PortfolioManager} />]
   }
 
   render() {
@@ -91,8 +92,6 @@ export default class App extends Component {
               loggedInStatus={this.state.loggedInStatus}
               handleSuccessfulLogout={this.handleSuccessfulLogout}  
             />
-
-            <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
               <Route exact path="/" component={Home} />
@@ -110,6 +109,7 @@ export default class App extends Component {
               
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
               {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null}
