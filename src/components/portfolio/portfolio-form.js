85deea9493efd9c3c9d1e6ddd39973bf4289918e
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-24 19:03:59 +0200

    G_25_updating_dropzone_labels_with_child_components_finalize

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -179,14 +179,14 @@ export default class PortfolioForm extends Component {
                 />
             </div>
 
-            <div className='image-uploaders three-column'>
+            <div className='image-uploaders'>
               <DropzoneComponent
                 ref={this.thumbRef}
                 config={this.componentConfig()}
                 djsConfig={this.djsConfig()}
                 eventHandlers={this.handleThumbDrop()}
               >
-
+                <div className='dz-message'>Thumbnail</div>
               </DropzoneComponent>
 
               <DropzoneComponent
@@ -195,7 +195,7 @@ export default class PortfolioForm extends Component {
                 djsConfig={this.djsConfig()}
                 eventHandlers={this.handleBannerDrop()}
               >
-
+                <div className='dz-message'>Banner</div>
               </DropzoneComponent>
 
               <DropzoneComponent
@@ -204,12 +204,12 @@ export default class PortfolioForm extends Component {
                 djsConfig={this.djsConfig()}
                 eventHandlers={this.handleLogoDrop()}
               >
-
+                <div className='dz-message'>Logo</div>
               </DropzoneComponent>
             </div>
 
             <div>
-              <button type='submit'>Save</button>
+              <button className='btn' type='submit'>Save</button>
             </div>
 
 
