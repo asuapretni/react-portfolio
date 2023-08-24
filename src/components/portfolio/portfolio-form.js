48ab8b8b2e3f8521bf25e63c6b6e9cd0f4adbca7
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-24 08:43:53 +0200

    G_19_uploading_thumbnail_images_to_the_api

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -24,8 +24,15 @@ export default class PortfolioForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.componentConfig = this.componentConfig.bind(this);
         this.djsConfig = this.djsConfig.bind(this);
+        this.handleThumbDrop = this.handleThumbDrop.bind(this);
     }
 
+  handleThumbDrop() {
+    return {
+      addedfile: file => this.setState({ thumb_image: file }) 
+    }
+  }
+  
   componentConfig() {
     return {
       iconFiletypes:[".jpg", ".png"],
@@ -49,6 +56,10 @@ export default class PortfolioForm extends Component {
     formData.append("portfolio_item[url]", this.state.url);
     formData.append("portfolio_item[category]", this.state.category);
     formData.append("portfolio_item[position]", this.state.position);
+
+    if (this.state.thumb_image) {
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+    }
 
     return formData;
   }  
@@ -131,8 +142,9 @@ export default class PortfolioForm extends Component {
               <DropzoneComponent
                 config={this.componentConfig()}
                 djsConfig={this.djsConfig()}
+                eventHandlers={this.handleThumbDrop()}
               >
-                
+
               </DropzoneComponent>
             </div>
 
