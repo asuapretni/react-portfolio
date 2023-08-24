7c89a5e6f9c773d8436219f8362b41f479734031
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-24 12:48:41 +0200

    G_22_Creating_form_input_mixin

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -128,9 +128,8 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-      <div>
-        <h1>PortfolioForm</h1>
-        <form onSubmit={this.handleSubmit}>
+
+        <form onSubmit={this.handleSubmit} className='portfolio-form-wrapper'>
             <div>
                 <input
                   type='text'
@@ -214,7 +213,7 @@ export default class PortfolioForm extends Component {
 
 
         </form>
-      </div>
+      
     );
     }
   }
