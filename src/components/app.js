c57dd919e2dc68846fe7e9394e5bd71819edae20
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-25 19:07:37 +0200

    H_06_Building_an_edit_click_handler

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import {
 import axios from 'axios';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import PortfolioManager from './pages/portfolio-manager';
 import NavigationContainer from "./navigation/navigation-container";
@@ -20,7 +20,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt);
+library.add(faTrash, faSignOutAlt, faEdit);
 
 export default class App extends Component {
   constructor(props) {
