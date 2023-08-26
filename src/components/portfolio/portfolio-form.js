c8c1f976f0b56c3d9e78fdfdaf982b84431ff5f9
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-08-26 18:25:22 +0200

    H_08_making_dynamic_axios_api_queries

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -18,7 +18,10 @@ export default class PortfolioForm extends Component {
             url: "",
             thumb_image: "",
             banner_image: "",
-            logo: ""
+            logo: "",
+            editMode: false,
+            apiUrl: "https://asuapretni.devcamp.space/portfolio/portfolio_items",
+            apiAction: "post"
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -47,7 +50,10 @@ export default class PortfolioForm extends Component {
         description: description || "",
         category: category || "eCommerce",
         position: position || "",
-        url: url || ""
+        url: url || "",
+        editMode: true,
+        apiUrl: `https://asuapretni.devcamp.space/portfolio/portfolio_items/${id}`,
+        apiAction: "patch"
       });
     }
   }
@@ -116,11 +122,12 @@ export default class PortfolioForm extends Component {
   }
 
   handleSubmit(event) {
-    axios.post(
-      "https://asuapretni.devcamp.space/portfolio/portfolio_items",
-      this.buildForm(),
-      { withCredentials: true }
-    ).then(response => {
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true
+    }).then(response => {
       this.props.handleSuccessfulFormSubmision(response.data.portfolio_item);
       
       this.setState({
