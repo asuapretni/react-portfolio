c2dfacb2d2aa46485c603ca1c987e82c7e86fab8
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-09-05 11:49:04 +0200

    I_13_getting_count_of_total_records_on_server

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -7,7 +7,9 @@ class Blog extends Component {
   constructor() {
     super();
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     }
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -23,12 +25,16 @@ class Blog extends Component {
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    })
     axios.get("https://asuapretni.devcamp.space/portfolio/portfolio_blogs",
     { withCredentials: true })
     .then(response => {
       this.setState({
-        blogItems: response.data.portfolio_blogs
-      })
+        blogItems: response.data.portfolio_blogs,
+        totalCount: response.data.meta.total_records
+      });
     })
     .catch(error => {
       console.log("getBlogItems error", error);
