4dfd220a7ec689002411d9d999e2345966e7f572
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-09-06 18:28:02 +0200

    I_21_applying_custom_styles_react_modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -4,11 +4,27 @@ import ReactModal from 'react-modal';
 export default class BlogModal extends Component {
   constructor(props) {
     super(props);
+
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "800px"
+      },
+
+      overlay: {
+        backgroundColor: "rgba(2, 89, 102, 0.9)"
+      }
+    };
   }
 
   render() {
     return (
       <ReactModal 
+        style={this.customStyles}
         onRequestClose={() => {
           this.props.handleModalClose();
         }}
