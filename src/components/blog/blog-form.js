f143aeb41a83392d3b20745bf531b8fb80ca5357
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2023-09-12 19:02:38 +0200

    I_27_applying_blog_form_styles

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -49,24 +49,25 @@ export default class BlogForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input 
-          type='text'
-          onChange={this.handleChange}
-          name='title'
-          placeholder='Blog title'
-          value={this.state.title}
-        />
-        
-        <input 
-          type='text'
-          onChange={this.handleChange}
-          name='blog_status'
-          placeholder='Blog status'
-          value={this.state.blog_status}
-        />
-
-        <button>Save</button>
+      <form onSubmit={this.handleSubmit} className='blog-form-wrapper'>
+        <div className='two-column'>
+          <input 
+            type='text'
+            onChange={this.handleChange}
+            name='title'
+            placeholder='Blog title'
+            value={this.state.title}
+          />
+          
+          <input 
+            type='text'
+            onChange={this.handleChange}
+            name='blog_status'
+            placeholder='Blog status'
+            value={this.state.blog_status}
+          />
+        </div>
+        <button className='btn'>Save</button>
       </form>
     );
     }
